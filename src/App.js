5baae774d9aa786a30b952fd64f466a53d7ac5bc
MrFarhan <muhammadfarhan012345@gmail.com>
2021-10-23 21:16:05 +0500

    react project working

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Hello world react application
+      </header>
+    </div>
+  );
+}
+
+export default App;
